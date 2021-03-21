4903c10c8952e12959357467c736859f38a325c0
seema thorat <seemagade97@gmail.com>
2021-03-21 19:27:47 +0530

    Add New Contact in Addresss Book UC-2 Done

---
+++ main/java/com/GetData.java
@@ -0,0 +1,16 @@
+package com;
+
+import java.util.Scanner;
+
+public class GetData
+{
+    private final static Scanner sc = new Scanner(System.in);
+    public static int getIntValue()
+    {
+        return sc.nextInt();
+    }
+    public static String getStringValue()
+    {
+        return sc.next();
+    }
+}
